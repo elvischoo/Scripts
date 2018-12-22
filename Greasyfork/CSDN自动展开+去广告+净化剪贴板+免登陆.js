0921005e0dc1ea605fc25bc12ae7be6ca5e3676c
elvis <elvischoo.pt@gmail.com>
2018-12-22 23:10:28 +0800

    update the CSDN js document

--- Greasyfork/CSDN自动展开+去广告+净化剪贴板+免登陆.js
+++ Greasyfork/CSDN自动展开+去广告+净化剪贴板+免登陆.js
@@ -1,34 +1,43 @@
 // ==UserScript==
-// @name         CSDN自动展开+去广告+净化剪贴板
+// @name         CSDN自动展开+去广告+净化剪贴板+免登陆
 // @namespace    http://tampermonkey.net/
-// @version      1.2.4
-// @description  自动展开阅读，可以将剪贴板的推广信息去除，去除大多数广告。
+// @version      1.2.6
+// @description  ITeye CSDN自动展开阅读，可以将剪贴板的推广信息去除，去除大多数广告。
 // @author       gorgias
 // @match        *://blog.csdn.net/*/article/details/*
 // @match        *://bbs.csdn.net/topics/*
+// @match        *://*.iteye.com/blog/*
 // @grant        none
 // @icon         https://csdnimg.cn/public/favicon.ico
 // @run-at       document-end
 // ==/UserScript==
 
+
 // 根据网速自己设置时间间隔
 var interval = 3000;
 var sideInterval = 4000;
 var bbsInterval = 3000; // 在ADBlock之后运行
+var iteyeInterval = 100;
+
 (function () {
     'use strict';
     var currentURL = window.location.href;
     var blog = /article/;
     var bbs = /topics/;
+    var iteye = /iteye/;
     //若为CSDN论坛,则：
     if(bbs.test(currentURL)){
         setTimeout(function () {
             $(".js_show_topic").click();
             document.getElementsByClassName("pulllog-box")[0].remove(); // 底部广告
-            console.log("removed");
+            $(".mediav_ad").remove();       // 帖子尾部广告
+            $(".post_recommend").remove();  // 帖子内[CSDN推荐]
         }, bbsInterval);
     }else if (blog.test(currentURL)){
-        if (document.getElementById("btn-readmore")){document.getElementById("btn-readmore").click();} //自动展开
+        if (document.getElementById("btn-readmore")){
+            document.getElementById("btn-readmore").click();
+            localStorage.setItem("anonymousUserLimit", "");
+        } //自动展开
         csdn.copyright.init("", "", ""); //去除剪贴板劫持
         setTimeout(function () {
             document.getElementsByClassName("csdn-tracking-statistics mb8 box-shadow")[0].remove(); //左上广告
@@ -61,6 +70,15 @@ var bbsInterval = 3000; // 在ADBlock之后运行
             for (var j = (recommend.length - 1); j >= 0; j--) {
                 recommend[j].remove();
             }
+            document.getElementsByClassName("fourth_column")[0].remove();
+        }, sideInterval);
+    } else if (iteye.test(currentURL)) {
+        setInterval(function(){
+            document.getElementById('btn-readmore').click();
+        }, iteyeInterval);
+        setTimeout(function () {
+            document.getElementsByClassName("blog-sidebar")[0].remove();
+            document.getElementById('main').style.width = '1000px';
         }, sideInterval);
     }
 })();
